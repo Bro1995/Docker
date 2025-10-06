6795ead75c1db74a226703cb77df73054a81a042
Baraa Alaswad <100236914+Bro1995@users.noreply.github.com>
2025-10-06 13:37:31 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Hello Docker 🚀</h1><p>This is a simple Flask app running inside a container!</p>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
